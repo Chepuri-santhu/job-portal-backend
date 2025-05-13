a424131cfbc302bf56e28bcf0d4461a168524148
Chepuri-santhu <152898624+Chepuri-santhu@users.noreply.github.com>
2025-05-13 23:33:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(CookieParser());
 
 // CORS configuration (only your deployed frontend)
 const corsOptions = {
-  origin: "https://job-portal-theta-lyart.vercel.app",
+  origin: "*",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
